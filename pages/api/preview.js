f1c5a5b45b0ed3b6a1171d5729d9b2e3d5ae79e7
kentico-michaelb <michaelb@kentico.com>
2021-12-29 15:30:35 -0500

    fixes WSL preview

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,20 +1,15 @@
+/* eslint-disable semi */
+/* eslint-disable quotes */
 import { getSitemapMappings } from "../../lib/api";
 import { getUrlFromMapping } from "../../utils";
 
 export default async function preview(req, res) {
-  // Check the secret and next parameters
-  // This secret should only be known to this API route and the CMS
-  if (
-    req.query.secret !== process.env.PREVIEW_SECRET
-  ) {
-    return res
-      .status(401)
-      .json({ message: "Invalid token" });
-  }
-
   console.log("Entering preview");
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({});
+
+  // THIS NEEDED TO BE ADDED
+  setCookieSameSite(res, "None");
 
   const redirectItemCodename = req.query.redirectItemCodename;
   if (redirectItemCodename) {
@@ -26,3 +21,18 @@ export default async function preview(req, res) {
 
   res.redirect("/");
 }
+
+const setCookieSameSite = (res, value) => {
+  const cookies = res.getHeader("Set-Cookie");
+  const updatedCookies = cookies?.map((cookie) =>
+    cookie.replace(
+      "SameSite=Lax",
+      `SameSite=${value}; Secure;`
+    )
+  )
+
+  res.setHeader(
+    "Set-Cookie",
+    updatedCookies
+  );
+};
